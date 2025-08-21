e3f1c51c0c1bc10102852db3b81fd00f382bf0b7
GianlorenzoLucioni <lucioni2002@gmail.com>
2025-08-21 16:36:47 +0200

    Update EsportatoreCsv.java

--- src/persistence/EsportatoreCsv.java
+++ src/persistence/EsportatoreCsv.java
@@ -1,4 +1,4 @@
-// Il file si trova in: src/persistence/EsportatoreCsv.java
+
 package persistence;
 
 import java.io.FileWriter;
@@ -15,19 +15,19 @@ public class EsportatoreCsv {
      * @param percorsoFile Il nome del file da creare (es. "bilancio.csv").
      */
     public void esportaRendiconto(List<MovimentoContabile> movimenti, String percorsoFile) {
-        // Usiamo un try-with-resources che chiude automaticamente il file anche in caso di errori.
+        
         try (PrintWriter writer = new PrintWriter(new FileWriter(percorsoFile))) {
 
-            // 1. Scriviamo l'intestazione delle colonne
+           
             writer.println("Data;Descrizione;Categoria;Entrate;Uscite");
 
-            // 2. Scriviamo una riga per ogni movimento
+           
             for (MovimentoContabile movimento : movimenti) {
-                // Usiamo il metodo toCsvString() che abbiamo intelligentemente preparato prima!
+                
                 writer.println(movimento.toCsvString());
             }
 
-            // 3. Calcoliamo e scriviamo i totali
+            
             double totaleEntrate = 0;
             double totaleUscite = 0;
             for (MovimentoContabile m : movimenti) {
@@ -39,11 +39,11 @@ public class EsportatoreCsv {
             }
             double avanzo = totaleEntrate - totaleUscite;
 
-            // Aggiungiamo qualche riga vuota per spaziare
+           
             writer.println();
             writer.println();
             
-            // Scriviamo il riepilogo finale
+          
             writer.println("RIEPILOGO; ; ;Totale Entrate;" + String.format("%.2f", totaleEntrate));
             writer.println(" ; ; ;Totale Uscite;" + String.format("%.2f", totaleUscite));
             writer.println(" ; ; ;AVANZO DI GESTIONE;" + String.format("%.2f", avanzo));
